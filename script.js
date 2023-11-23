4e750c420a3d02704b03031e192cbb0a49543411
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2023-11-23 16:07:48 +0530

    First development of Calculator Completed

---
+++ script.js
@@ -0,0 +1,28 @@
+let display_input = "";
+
+function display(input) {
+  display_input = display_input.toString();
+  if (input === "←") {
+    if (display_input.length === 0) {
+      display_input = "";
+    } else {
+      display_input = display_input.slice(0, -1);
+      console.log(display_input);
+    }
+
+    document.querySelector(".display input").value = display_input;
+  } else if (input === "C") {
+    document.querySelector(".display input").value = "";
+    display_input = "";
+  } else if (input === "=") {
+    try {
+      display_input = eval(display_input);
+      document.querySelector(".display input").value = display_input;
+    } catch (err) {
+      document.querySelector(".display input").value = "ERROR";
+    }
+  } else {
+    display_input += input;
+    document.querySelector(".display input").value = display_input;
+  }
+}
